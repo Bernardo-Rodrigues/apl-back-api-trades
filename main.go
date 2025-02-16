5bf52329722299c052bc5f452d41ac00e4260d0f
Bernardo Rodrigues <bernardo.rodrigues06@unifei.edu.br>
2025-02-16 07:59:08 -0300

    refactor: separing into files

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"encoding/csv"
 	"fmt"
 	"io"
 	"os"
-	"strconv"
-	"sync"
 	"time"
 )
 
@@ -46,45 +43,6 @@ func main() {
 	fmt.Printf("Execution time: %v µs\n", time.Since(start).Microseconds())
 }
 
-func loadValues(startDate, endDate time.Time, tradesFile io.Reader, assetsFiles map[string]io.Reader) ([]Trade, map[time.Time]map[string]float64) {
-	chTrades := make(chan Trade, 100)
-	chPrices := make(chan map[time.Time]map[string]float64, len(assetsFiles))
-	var wg sync.WaitGroup
-
-	wg.Add(1)
-	go loadTrades(tradesFile, startDate, endDate, chTrades, &wg)
-
-	for assetName, assetFile := range assetsFiles {
-		wg.Add(1)
-		go loadPrices(assetFile, assetName, startDate, endDate, chPrices, &wg)
-	}
-
-	go func() {
-		wg.Wait()
-		close(chTrades)
-		close(chPrices)
-	}()
-
-	trades := []Trade{}
-	for trade := range chTrades {
-		trades = append(trades, trade)
-	}
-
-	prices := make(map[time.Time]map[string]float64)
-	for assetsPriceAtInstant := range chPrices {
-		for instant, assetsPrice := range assetsPriceAtInstant {
-			if _, ok := prices[instant]; !ok {
-				prices[instant] = make(map[string]float64)
-			}
-			for assets, price := range assetsPrice {
-				prices[instant][assets] = price
-			}
-		}
-	}
-
-	return trades, prices
-}
-
 func generateReport(trades Trades, prices map[time.Time]map[string]float64, startDate, endDate time.Time, minutesInterval int, initialBalance float64) {
 	cashBalance := initialBalance
 	fmt.Printf("%s\t%.4f\t%.5f\n", startDate.Format("2006-01-02 15:04:05"), cashBalance, 0.0)
@@ -110,152 +68,4 @@ func generateReport(trades Trades, prices map[time.Time]map[string]float64, star
 
 		fmt.Printf("%s\t%.4f\t%.5f\n", intervalEnd.Format("2006-01-02 15:04:05"), totalBalance, accumulatedProfit)
 	}
-}
-
-func calculateCashBalancePerInterval(trades []Trade, assets map[string]int) float64 {
-	cash := 0.0
-
-	for _, t := range trades {
-		if t.TradesType == "BUY" {
-			assets[t.AssetName] += t.AssetQuantity
-			cash -= float64(t.AssetQuantity) * t.AssetPrice
-			fmt.Printf("[%s] Bought %d units of %s, spent $%.2f\n", t.Date.Format("2006-01-02 15:04:05"), t.AssetQuantity, t.AssetName, float64(t.AssetQuantity)*t.AssetPrice)
-		} else if t.TradesType == "SELL" {
-			assets[t.AssetName] -= t.AssetQuantity
-			cash += float64(t.AssetQuantity) * t.AssetPrice
-			fmt.Printf("[%s] Sold %d units of %s, recovered $%.2f\n", t.Date.Format("2006-01-02 15:04:05"), t.AssetQuantity, t.AssetName, float64(t.AssetQuantity)*t.AssetPrice)
-		}
-	}
-
-	return cash
-}
-
-func calculateAssetsValueAtIntervalEnd(prices map[time.Time]map[string]float64, end time.Time, assets map[string]int) float64 {
-	assetsValue := 0.0
-	for asset, quantity := range assets {
-		assetValue := getInstantPrice(prices, asset, end)
-		assetsValue += float64(quantity) * assetValue
-		fmt.Printf("[%s] Asset %s with %d units valued at $%.2f each, total $%.2f\n", end.Format("2006-01-02 15:04:05"), asset, quantity, assetValue, float64(quantity)*assetValue)
-	}
-
-	return assetsValue
-}
-
-func getInstantPrice(prices map[time.Time]map[string]float64, asset string, instante time.Time) float64 {
-	truncateInstant := time.Date(instante.Year(), instante.Month(), instante.Day(), instante.Hour(), instante.Minute(), 0, 0, instante.Location())
-
-	assets := prices[truncateInstant]
-	return assets[asset]
-}
-
-func loadTrades(file io.Reader, start, end time.Time, ch chan<- Trade, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	reader := csv.NewReader(file)
-	layout := "2006-01-02 15:04:05"
-
-	_, err := reader.Read()
-	if err != nil {
-		fmt.Println("Error reading trades file headers:", err)
-	}
-
-	for {
-		line, err := reader.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			fmt.Println("Error reading trades file line:", err)
-			break
-		}
-
-		date, _ := time.Parse(layout, line[0])
-		if date.Before(start) {
-			continue
-		}
-		if date.After(end) {
-			break
-		}
-
-		quantity, _ := strconv.Atoi(line[2])
-		price, _ := strconv.ParseFloat(line[3], 64)
-
-		trade := Trade{
-			Date:          date,
-			AssetName:     line[1],
-			AssetQuantity: quantity,
-			AssetPrice:    price,
-			TradesType:    line[4],
-		}
-
-		ch <- trade
-	}
-}
-
-func loadPrices(file io.Reader, asset string, start, end time.Time, ch chan<- map[time.Time]map[string]float64, wg *sync.WaitGroup) {
-	defer wg.Done()
-	reader := csv.NewReader(file)
-
-	prices := make(map[time.Time]map[string]float64)
-	layout := "2006-01-02 15:04:05"
-
-	_, err := reader.Read()
-	if err != nil {
-		fmt.Println("Error reading prices file headers:", err)
-	}
-
-	for {
-		line, err := reader.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			fmt.Println("Error reading prices file line:", err)
-			break
-		}
-
-		date, _ := time.Parse(layout, line[0])
-
-		if date.Before(start) {
-			continue
-		}
-		if date.After(end) {
-			break
-		}
-
-		price, _ := strconv.ParseFloat(line[1], 64)
-
-		if _, ok := prices[date]; !ok {
-			prices[date] = make(map[string]float64)
-		}
-		prices[date][asset] = price
-	}
-
-	ch <- prices
-}
-
-type Trade struct {
-	Date          time.Time
-	AssetName     string
-	AssetQuantity int
-	AssetPrice    float64
-	TradesType    string
-}
-
-type Trades []Trade
-
-func (trades Trades) FilterInInterval(start, end time.Time) []Trade {
-	filteredTrades := make([]Trade, 0)
-
-	for _, t := range trades {
-		if t.Date.Before(start) {
-			continue
-		}
-		if t.Date.After(end) {
-			break
-		}
-		filteredTrades = append(filteredTrades, t)
-	}
-
-	return filteredTrades
 }
